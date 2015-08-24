109dbe28637422a698e1f1f4a97b2f0904c4c292
EJLarcs <ejlarcombe@gmail.com>
2015-08-24 09:43:12 -0400

    done

--- 003.a.magic.rack.app/app.rb
+++ 003.a.magic.rack.app/app.rb
@@ -19,10 +19,10 @@ require 'twitter'
 
 TWITTER = Twitter::REST::Client.new do |config|
   # Go to https://dev.twitter.com/apps and create a new twitter application and generate these keys and tokens
-  config.consumer_key = "consumer key here"
-  config.consumer_secret = "consumer secret here"
-  config.access_token = "access token here"
-  config.access_token_secret = "token secret here"
+  config.consumer_key = "GRUsb21O1zgxIBOHneZlGZTki"
+  config.consumer_secret = "4x0qaoDC05QE7PtbTs8gIxmKsg72lwMdqwjABIJcF58gEPsfgk"
+  config.access_token = "3010889835-dX3B52r2sQcOlIsyyS9bmTEXBgiU2vpv6Rhfxq9"
+  config.access_token_secret = "6c7j8WmPt4Vux3TmVOUb3WY28OrhlCyE1wKN01dKeG1t9"
 end
 
 # What do you think is going on up there? Well the twitter gem gives us a class,
@@ -36,7 +36,7 @@ class App
     html = "<h1>What people are saying about Flatiron School</h1>"
     html << "<ul>"
 
-    # So far we've created an HTML string. Now the fun part. 
+    # So far we've created an HTML string. Now the fun part.
     # Let's search twitter.
     twitter_search_results = TWITTER.search("flatironschool")
     twitter_search_results.each do |tweet|
